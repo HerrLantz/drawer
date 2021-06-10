02f52495293088ec57351d149d9653ee4de965e2
Patric Lantz <patricla@kth.se>
2021-06-10 14:48:07 +0200

    Initial commit

---
+++ src/drawer/core.cljs
@@ -0,0 +1,10 @@
+(ns ^:figwheel-hooks drawer.core
+  (:require
+   [goog.dom :as gdom]
+   [reagent.core :as reagent :refer [atom]]
+   [reagent.dom :as rdom]))
+
+(defn hello-world []
+  [])
+
+
---
+++ test/drawer/core_test.cljs
@@ -0,0 +1,4 @@
+(ns drawer.core-test
+  (:require
+   [cljs.test :refer-macros [deftest is testing]]))
+
